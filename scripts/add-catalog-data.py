8f915c09e8c781144980671bfb05a3772e591d22
Joe Clarke <jclarke@cisco.com>
2016-11-12 20:50:46 -0500

    Use Benoit's JSON data to fill in maturity.

--- scripts/add-catalog-data.py
+++ scripts/add-catalog-data.py
@@ -25,46 +25,53 @@
 # SUCH DAMAGE.
 
 import sqlite3
+import requests
 import re
 import os
 import sys
 
 # Script should be called with the following arguments
-#  Module file  : fully-qualified path to the YANG module
-#  Module name  : Name of YANG module
 #  Database file: SQLite database file
 
-if len(sys.argv) != 4:
-    print("Usage: {} <mod path> <mod name> <DB file>".format(sys.argv[0]))
+if len(sys.argv) != 2:
+    print("Usage: {} <DB file>".format(sys.argv[0]))
     sys.exit(1)
 
-modf = sys.argv[1]
-modn = sys.argv[2]
-dbf = sys.argv[3]
+dbf = sys.argv[1]
 
 # Map propietary namespaces to known org strings.
 NS_MAP = {
     "http://cisco.com/ns/yang/": "cisco"
 }
 
-DIR_MAP = {
-    "rfc": "RFC",
-    "draft": "DRAFT"
+mods = {}
+
+MATURITY_MAP = {
+    "rfc": "http://www.claise.be/IETFYANGNoRevRFC.json",
+    "draft": "http://www.claise.be/IETFYANGNoRevDraft.json"
 }
 
-maturity = 'N/A'
-
-# XXX This is a hack as the catalog does not yet list maturity.
-# This gives us RFC status for the IETF, but nothing else.
-dirs = os.path.normpath(os.path.dirname(modf)).split(os.sep)
-for dk, dv in DIR_MAP.items():
-    for d in dirs:
-        if re.search(dk, d, re.I):
-            maturity = dv
-            break
-    else:
+for m, u in MATURITY_MAP.items():
+    try:
+        r = requests.request("GET", u)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print("Error fetching JSON data from {}: {}".format(u, e.args[0]))
         continue
-    break
+
+    j = r.json()
+
+    for mod, props in j.items():
+        mods[mod]['maturity'] = m
+        reg = re.compile(r'<a.*?>(.*?)</a>', re.S | re.M)
+        doc_tag = props
+        if isinstance(props, list):
+            doc_tag = props[0]
+        match = reg.match(doc_tag)
+        if match:
+            mods[mod]['document'] = match.groups()[0].strip()
+        else:
+            mods[mod]['document'] = ''
 
 try:
     con = sqlite3.connect(dbf)
@@ -73,33 +80,23 @@ except sqlite3.Error as e:
     print("Error connecting to DB: {}".format(e.args[0]))
     sys.exit(1)
 
-sql = 'UPDATE modules SET maturity=:maturity WHERE module=:modn'
-try:
-    cur.execute(sql, {'maturity': maturity, 'modn': modn})
-    con.commit()
-except sqlite3.Error as e:
-    print("Failed to update module maturity for {}: {}".format(
-        modn, e.args[0]))
-    sys.exit(1)
+for modn, props in mods.items():
+    sql = 'UPDATE modules SET maturity=:maturity, document=:document WHERE module=:modn'
+    try:
+        cur.execute(sql, {'maturity': props['maturity'], 'document': props[
+                    'document'], 'modn': modn})
+        con.commit()
+    except sqlite3.Error as e:
+        print("Failed to update module maturity for {}: {}".format(
+            modn, e.args[0]))
+        sys.exit(1)
 
-sql = 'SELECT namespace FROM modules WHERE module=:modn'
-try:
-    cur.execute(sql, {'modn': modn})
-except sqlite3.Error as e:
-    print("Failed to get module data for {}: {}".format(modn, e.args[0]))
-    sys.exit(1)
-
-ns = cur.fetchone()[0]
-for n, o in NS_MAP.items():
-    if re.search(n, ns, re.I):
-        sql = 'UPDATE modules SET organization=:org WHERE module=:modn'
-        try:
-            cur.execute(sql, {'org': o, 'modn': modn})
-            con.commit()
-        except sqlite3.Error as e:
-            print("Failed to update organization for {}: {}".format(
-                modn, e.args[0]))
-            sys.exit(1)
-        break
+for ns, org in NS_MAP.items():
+    sql = 'UPDATE modules SET organization=:org WHERE namespace LIKE :ns'
+    try:
+        cur.execute(sql, {'org': org, 'ns': ns + '%'})
+    except sqlite3.Error as e:
+        print("Failed to get module data for {}: {}".format(modn, e.args[0]))
+        sys.exit(1)
 
 con.close()
