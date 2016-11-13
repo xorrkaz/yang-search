c2a632406248366440e90ad874edcd5f2201c94e
Joe Clarke <jclarke@cisco.com>
2016-11-12 23:46:32 -0500

    Fix typo.

--- www/impact_analysis.php
+++ www/impact_analysis.php
@@ -186,7 +186,7 @@ if (!isset($_GET['modules'])) {
     if (isset($_GET['recurse']) && is_numeric($_GET['recurse'])) {
         $recurse = $_GET['recurse'];
     }
-    if (isset($_GET['rfcs']) && $_GET['show_rfcs'] == 0) {
+    if (isset($_GET['rfcs']) && $_GET['rfcs'] == 0) {
         $show_rfcs = false;
     }
     foreach ($modules as $module) {
