c9b0f4002438eeaca2ab88fd487b8d90b6691b7f
Joe Clarke <jclarke@cisco.com>
2017-08-17 09:00:57 -0400

    Take care of a left over variable.

--- www/yang_catalog.inc.php
+++ www/yang_catalog.inc.php
@@ -239,7 +239,7 @@ function get_maturity($mod_obj, &$alerts)
             }
         }
     } catch (Exception $e) {
-        push_exception("Failed to get module maturity for $module (perhaps it doesn't validate?)", $e, $alerts);
+        push_exception("Failed to get module maturity for {$mod_obj->getName()}@{$mod_obj->getRevision()}/{$mod_obj->getOrganization()} (perhaps it doesn't validate?)", $e, $alerts);
     }
 
     return $maturity;
