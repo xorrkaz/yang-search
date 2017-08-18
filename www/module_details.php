b64028369b55a8b3b084b25aa5c0b99fd99c773b
Joe Clarke <jclarke@cisco.com>
2017-08-18 15:52:48 -0400

    Fix a typo.

--- www/module_details.php
+++ www/module_details.php
@@ -298,7 +298,7 @@ foreach ($alerts as $alert) {
               ?>
         <tr>
           <td style="text-align: right"><b><?=$key?></b></td>
-          <td><?=(str_replace("\n", "<br/>\n", htmlentities($val))?></td>
+          <td><?=(str_replace("\n", "<br/>\n", htmlentities($val)))?></td>
         </tr>
         <?php
 
