da559e5c3d79eeb0979c1a0309e6d183cba1d39e
Joe Clarke <jclarke@cisco.com>
2017-09-05 11:34:37 -0400

    Start to hook multiple tools up to search results.

--- www/yang-search.php
+++ www/yang-search.php
@@ -326,7 +326,11 @@ if (isset($_POST['search_string'])) {
 <?php
             if ((isset($modules[$res_mod['sig']]) && $modules[$res_mod['sig']] === true) || (!isset($modules[$res_mod['module']]) && is_file(YTREES_DIR.'/'.$res_mod['module'].'@'.$res_mod['revision'].'.json'))) {
                 ?>
-            <td><a href="yang_tree.php?module=<?=$res_mod['module']?>"><?=$res_mod['module']?></a> <span style="font-size: small">(<a href="impact_analysis.php?modules[]=<?=$res_mod['module']?>">Impact Analysis</a>)</span></td>
+            <td><?=$res_mod['module']?> <span style="font-size: small">(<a href="yang_tree.php?module=<?=$res_mod['module']?>"><img border="0" src="img/leaf.png" title="Tree View for <?=$res_mod['module']?>">
+              Tree View</a>
+              |
+              <a href="impact_analysis.php?modules[]=<?=$res_mod['module']?>"><img src="img/impact.png" border="0" title="Impact Analysis for <?=$res_mod['module']?>">
+                Impact Analysis</a>)</span></td>
 <?php
                 $modules[$res_mod['sig']] = true;
             } else {
