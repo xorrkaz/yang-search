6e280fb3e672b4502c00ce36c3cf2840f372fd8e
Joe Clarke <jclarke@cisco.com>
2017-10-03 10:00:23 -0400

    We don't need to include this here.

--- www/Rester.php
+++ www/Rester.php
@@ -24,7 +24,7 @@
 // OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
 
-include_once 'yang_catalog.inc.php';
+//include_once 'yang_catalog.inc.php';
 require_once 'httpful.phar';
 
 class RestException extends RuntimeException
